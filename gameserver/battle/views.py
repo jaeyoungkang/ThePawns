0050cdf5577822e347aef536f0cfeeb66cc8a362
jaeyoungkang <jaeyoung2010@gmail.com>
2016-04-07 23:44:15 +0900

    서버에 각 말들 객체만들고 위지 정보 저장, 파란 말만 보여지게 작업

--- gameserver/battle/views.py
+++ gameserver/battle/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render, HttpResponse
 
-info_of_map = "00 00 22 22"
+pawn_blue = 0
+obsrver_blue = 0
+pawn_red = 22
+observer_red = 22
 
 def get_info_map(request):
-    response = HttpResponse(info_of_map)
+    fommat_of_map = str(pawn_blue) + " " + str(obsrver_blue) +" " + str(pawn_red) + " " + str(observer_red)
+    response = HttpResponse(fommat_of_map )
     return response
 
 def post_info_map(request):
-    global info_of_map
     response = HttpResponse('')
     if request.method == 'POST':
         print(request.POST['info'])
-        info_of_map = request.POST['info']
+        update_info(request.POST['info'])
     return response
+
+def update_info(fommat_of_map):
+    global pawn_blue
+    global obsrver_blue
+    global pawn_red
+    global observer_red
+
+    infos = fommat_of_map.splite(' ')
+    print(infos)
+
+    pawn_blue = infos[0]
+    obsrver_blue = infos[1]
+    pawn_red = infos[2]
+    observer_red = infos[3]
