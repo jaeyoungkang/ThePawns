8208f5ce92118976e12e1aa85c0581d95e22a41d
jaeyoungkang <jaeyoung2010@gmail.com>
2016-04-07 23:12:15 +0900

    위치정보 서버에 post get

--- gameserver/battle/views.py
+++ gameserver/battle/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
+info_of_map = "00 00 22 22"
+
 def get_info_map(request):
-    response = HttpResponse('11 11 33 33')
+    response = HttpResponse(info_of_map)
     return response
 
 def post_info_map(request):
+    global info_of_map
     response = HttpResponse('')
     if request.method == 'POST':
         print(request.POST['info'])
+        info_of_map = request.POST['info']
     return response
