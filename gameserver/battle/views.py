daadad6d457fd6a59d075f564a4cb24cf0e37123
jaeyoungkang <jaeyoung2010@gmail.com>
2016-04-08 18:17:54 +0900

    현재 자신이 볼수 잇는 말만 보는 기능 작업

--- gameserver/battle/views.py
+++ gameserver/battle/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, HttpResponse
 
 pawn_blue = 0
-obsrver_blue = 0
+observer_blue = 0
 pawn_red = 22
 observer_red = 22
 
 def get_info_map(request):
-    fommat_of_map = str(pawn_blue) + " " + str(obsrver_blue) +" " + str(pawn_red) + " " + str(observer_red)
-    response = HttpResponse(fommat_of_map )
+    print('GET : ' + str(pawn_blue) + " " + str(observer_blue) + " " + str(pawn_red) + " " +  str(observer_red) )
+
+    fommat_of_map = ""
+    fommat_of_map += str(pawn_blue) + " "
+    fommat_of_map += str(observer_blue) + " "
+    if observer_blue == pawn_red:
+        fommat_of_map += str(pawn_red) + " "
+    else:
+        fommat_of_map += "-1" + " "
+
+    fommat_of_map += "-1" + " "
+
+    response = HttpResponse(fommat_of_map)
+    print('GET : ' + str(fommat_of_map))
     return response
 
 def post_info_map(request):
@@ -19,14 +31,10 @@ def post_info_map(request):
 
 def update_info(fommat_of_map):
     global pawn_blue
-    global obsrver_blue
-    global pawn_red
-    global observer_red
+    global observer_blue
 
-    infos = fommat_of_map.splite(' ')
-    print(infos)
+    infos = fommat_of_map.split(' ')
+    pawn_blue = int(infos[0])
+    observer_blue = int(infos[1])
+    print('POSTED : '+ str(infos))
 
-    pawn_blue = infos[0]
-    obsrver_blue = infos[1]
-    pawn_red = infos[2]
-    observer_red = infos[3]
