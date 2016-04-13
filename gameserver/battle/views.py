c524cf04555ec934f4b5c589aac27c7255036134
jaeyoungkang <jaeyoung2010@gmail.com>
2016-04-13 19:04:27 +0900

    상대편 표시 및 이동 처리

--- gameserver/battle/views.py
+++ gameserver/battle/views.py
@@ -6,35 +6,54 @@ pawn_red = 22
 observer_red = 22
 
 def get_info_map(request):
-    print('GET : ' + str(pawn_blue) + " " + str(observer_blue) + " " + str(pawn_red) + " " +  str(observer_red) )
-
+    my_team = request.GET['team']
     fommat_of_map = ""
-    fommat_of_map += str(pawn_blue) + " "
-    fommat_of_map += str(observer_blue) + " "
-    if observer_blue == pawn_red:
+
+    print('GET : ' + my_team)
+    if my_team  == 'BLUE':
+        fommat_of_map += str(pawn_blue) + " "
+        fommat_of_map += str(observer_blue) + " "
+        if observer_blue == pawn_red:
+            fommat_of_map += str(pawn_red) + " "
+        else:
+            fommat_of_map += "-1" + " "
+
+    elif my_team == 'RED':
         fommat_of_map += str(pawn_red) + " "
-    else:
-        fommat_of_map += "-1" + " "
+        fommat_of_map += str(observer_red) + " "
+        if observer_red == pawn_blue:
+            fommat_of_map += str(pawn_blue) + " "
+        else:
+            fommat_of_map += "-1" + " "
 
-    fommat_of_map += "-1" + " "
 
-    response = HttpResponse(fommat_of_map)
     print('GET : ' + str(fommat_of_map))
+    response = HttpResponse(fommat_of_map)
     return response
 
 def post_info_map(request):
     response = HttpResponse('')
     if request.method == 'POST':
         print(request.POST['info'])
-        update_info(request.POST['info'])
+        update_info(request.POST['info'], request.POST['team'])
     return response
 
-def update_info(fommat_of_map):
+def update_info(fommat_of_map, my_team):
     global pawn_blue
     global observer_blue
+    global pawn_red
+    global observer_red
 
     infos = fommat_of_map.split(' ')
-    pawn_blue = int(infos[0])
-    observer_blue = int(infos[1])
+
+    if my_team == 'BLUE':
+        infos = fommat_of_map.split(' ')
+        pawn_blue = int(infos[0])
+        observer_blue = int(infos[1])
+    elif my_team == 'RED':
+        infos = fommat_of_map.split(' ')
+        pawn_red= int(infos[0])
+        observer_red = int(infos[1])
+
     print('POSTED : '+ str(infos))
 
